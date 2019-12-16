e4d61240493bf2d45b4747354c4724bf7627255b
anthony <anthonyb921@gmail.com>
2019-12-15 18:21:45 -0800

    added guest login to handy-helper

--- handy-helper/apps/login/urls.py
+++ handy-helper/apps/login/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^register$',views.register,name="register"),
     url(r'^login$',views.login,name="login"),
     url(r'^logout$',views.logout,name="logout"),
-    url(r'^dashboard$',views.dashboard,name="dashboard")
+    url(r'^dashboard$',views.dashboard,name="dashboard"),
+    url(r'^guest$',views.guest,name="guest")
 ] 
